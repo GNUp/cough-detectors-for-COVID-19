55d507d309f192986c43f1b85be9976328461eba
Geunwoo Kim <posgnu@gmail.com>
2021-01-13 02:43:10 +0900

    2021-01-13

--- ex.sh
+++ ex.sh
@@ -1,1 +1,3 @@
-python human_pose_estimation.py -m intel/human-pose-estimation-0001/FP16/human-pose-estimation-0001.xml -d MYRIAD -at openpose
+source ~/openvino/openvino_dist/bin/setupvars.sh
+python3 human_pose_estimation.py -m intel/human-pose-estimation-0001/FP16/human-pose-estimation-0001.xml -d MYRIAD -at openpose
+
